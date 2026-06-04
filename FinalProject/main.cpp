5678f922a60b6cdd68bf6325f0be89d5ed9376f2
kangkang0430 <2442965055@qq.com>
2026-06-04 20:06:45 +0800

    Add files via upload

---
+++ FinalProject/main.cpp
@@ -0,0 +1,16 @@
+// Your implementation of Fluid Simulation.
+
+#include "Assets/bundled.h"
+#include "Labs/FinalProject/App.h"
+
+int main() {
+    // make linker happy
+    using namespace VCX;
+    return Engine::RunApp<Labs::FluidSimulation::App>(Engine::AppContextOptions {
+        .Title         = "VCX-sim Lab4: Final Project - Coupled Fluid-Rigid Simulation",
+        .WindowSize    = {1024, 768},
+        .FontSize      = 16,
+        .IconFileNames = Assets::DefaultIcons,
+        .FontFileNames = Assets::DefaultFonts,
+    });
+}
